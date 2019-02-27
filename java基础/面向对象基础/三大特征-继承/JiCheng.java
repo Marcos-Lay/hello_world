9c6a04f30e76517c6a12ffe90d889815067e638e
马磊 <malei_fb@cfets.zh>
2019-02-27 16:34:45 +0800

    面向对象-继承

---
+++ java基础/面向对象基础/三大特征-继承/JiCheng.java
@@ -0,0 +1,21 @@
+/* 
+* @Author: 马磊  
+* @Date: 2019-02-27 14:45:58  
+* @Last Modified by:   马磊  * @Last Modified time: 2019-02-27 14:45:58  */
+//继承 关键字 entends
+public class JiCheng{
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.shut();
+    }
+}
+class AnimalJC{
+    String name;
+    Integer age;
+    public void shut(){
+        System.out.println("主类");
+    }
+}
+class Cat extends AnimalJC{
+
+}
